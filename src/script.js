338ee92cb66df436afb255ad761faaaab2b85e76
Ana-Almeida110 <coiadoalmeida@gmail.com>
2024-03-01 21:36:15 -0300

    adição de logo Linkedin/GitHub e identação

--- src/script.js
+++ src/script.js
@@ -4,22 +4,22 @@ var outInput = document.querySelector('.mensagem');
 function criptografar() {
     let texto = textInput.value;
     let resultadoCripto = texto
-    .replace(/e/g, 'enter')
-    .replace(/i/g, 'imes')
-    .replace(/a/g, 'ai')
-    .replace(/o/g, 'ober')
-    .replace(/u/g, 'ufat'); 
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
     document.getElementById("mensagem_criptografada").value = resultadoCripto;
 }
 
 function descriptografar() {
     let texto = textInput.value;
     let resultadoDescripto = texto
-    .replace(/enter/g, 'e')
-    .replace(/imes/g, 'i')
-    .replace(/ai/g, 'a')
-    .replace(/ober/g, 'o')
-    .replace(/ufat/g, 'u');
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
     document.getElementById("mensagem_criptografada").value = resultadoDescripto;
 }
 
@@ -31,4 +31,3 @@ function copiar() {
     textoCopia.value = ' ';
 }
 
- 
